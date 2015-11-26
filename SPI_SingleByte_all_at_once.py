da0cb7f37cf09e0dcd939453ef1dacc3cd49d279
Ryan Krauss <ryanlists@gmail.com>
2015-11-26 01:32:45 -0600

    trying to understand

--- SPI_SingleByte_all_at_once.py
+++ SPI_SingleByte_all_at_once.py
@@ -15,18 +15,21 @@ spi.max_speed_hz = 4000000
 
 t1 = time.time()
 
-#ilist = range(1,10)
-ilist = [1,0,2,0,3,0,4,0,5,0,6,0,7,0,8,0,9,0]
+ilist = range(1,10)
+#ilist = [1,0,2,0,3,0,4,0,5,0,6,0,7,0,8,0,9,0]
 N = len(ilist)
 #responses = zeros(N, int)
 
 
-responses = spi.xfer(ilist,4000000,10)
-## for i in ilist:
-##     n = i
-##     data = spi.xfer([n,0])
-##     n_echo = data[0]
-##     responses[i-1] = n_echo
+#responses = spi.xfer(ilist,4000000,10)
+responses = []
+
+for i in ilist:
+    n = i
+    data = spi.xfer([n,0])
+    #n_echo = data[0]
+    #responses[i-1] = n_echo
+    responses.append(data)
 
 #responses = concatenate((responses[1:],[responses[0]]), axis = 0)
 
